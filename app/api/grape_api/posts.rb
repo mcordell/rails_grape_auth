8bc43d360c41b5e52c3ce5bae0aca7070a252493
Michael Cordell <surpher@gmail.com>
2015-06-10 22:00:39 -0700

    Add basic auth class into posts

--- app/api/grape_api/posts.rb
+++ app/api/grape_api/posts.rb
@@ -1,5 +1,21 @@
+class AuthMiddleware
+  def initialize(app, args)
+    @app = app
+    @args = args
+  end
+
+  def call(env)
+    binding.pry
+    return @app.call(env)
+  end
+end
+
+Grape::Middleware::Auth::Strategies.add(:my_auth, AuthMiddleware, ->(options) { [options[:realm]] } )
+
 module GrapeApi
   class Posts < Grape::API
+    auth :my_auth, { realm: 'Test Api'}
+
     format :json
 
     get '/' do
